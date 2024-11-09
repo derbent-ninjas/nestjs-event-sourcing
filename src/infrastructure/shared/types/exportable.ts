8241b7e9d3124fc20fae46b1a799642e462271aa
TbotaPhantA <botakiller@mail.ru>
2024-11-09 15:42:44 +0000

    useful types were added

---
+++ src/infrastructure/shared/types/exportable.ts
@@ -0,0 +1,5 @@
+export interface Exportable<
+  T extends Record<string, any> = Record<string, any>,
+> {
+  export(): T;
+}
---
+++ src/infrastructure/shared/types/importable.ts
@@ -0,0 +1,5 @@
+export interface Importable<
+  T extends Record<string, any> = Record<string, any>,
+> {
+  import(data: T): void;
+}
