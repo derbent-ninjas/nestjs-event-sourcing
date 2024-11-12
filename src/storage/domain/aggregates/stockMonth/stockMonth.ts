05b14dbb8a297ad44c176cb53e42a8ca5854401a
TbotaPhantA <botakiller@mail.ru>
2024-11-12 22:34:20 +0000

    stockMonth aggregate refactoring

--- src/storage/domain/aggregates/stockMonth/stockMonth.ts
+++ src/storage/domain/aggregates/stockMonth/stockMonth.ts
@@ -1,36 +1,43 @@
 import { StockItem } from './stockItem';
-import { AggregateRoot } from '../AggregateRoot';
-import { Event } from '../../events/event';
-import { StockMonthWasOpenedEventData } from '../../events/openStockMonthEventData';
+import { AggregateRoot, DefaultAggregateData } from '../AggregateRoot';
 import { exhaustiveTypeException } from 'tsconfig-paths/lib/try-path';
+import { StockMonthWasOpened } from '../../events/stockMonthWasOpened';
+import { ItemsWereReceived } from '../../events/itemsWereReceived';
+
+type AllEventTypes = StockMonthWasOpened | ItemsWereReceived;
 
 export class StockMonth extends AggregateRoot<StockMonthData> {
-  transform(_event: Event): void {
-    if (StockMonthWasOpenedEventData.isThisEvent(_event)) {
-      this.transformStockMonthWasOpened(_event);
+  transform(event: AllEventTypes): void {
+    if (event instanceof StockMonthWasOpened) {
+      return this.transformStockMonthWasOpened(event);
+    } else if (event instanceof ItemsWereReceived) {
+      this.transformItemsWereReceived(event);
     } else {
-      exhaustiveTypeException(_event);
+      exhaustiveTypeException(event);
     }
   }
 
-  private transformStockMonthWasOpened(
-    _event: Event<StockMonthWasOpenedEventData>,
-  ) {
+  private transformStockMonthWasOpened(event: StockMonthWasOpened) {
     try {
-      this.__data.aggregateId = _event.aggregateId;
-      this.__data.aggregateVersion = _event.version;
-      this.__data.items = _event.data.items;
-      this.__data.quantity = _event.data.items.length;
+      this.__data.aggregateId = event.aggregateId;
+      this.__data.aggregateVersion = event.version;
+      this.__data.items = event.data.items;
     } catch (e) {
-      console.log(`Error processing StockMonthWasOpenedEvent: ${e}`);
+      console.log(`Error processing ${StockMonthWasOpened.name}: ${e}`);
+      throw e;
+    }
+  }
+
+  private transformItemsWereReceived(_event: ItemsWereReceived) {
+    try {
+      this.__data.items.push(..._event.data.items);
+    } catch (e) {
+      console.log(`Error processing ${ItemsWereReceived.name}: ${e}`);
       throw e;
     }
   }
 }
 
-interface StockMonthData {
-  aggregateId: string;
-  aggregateVersion: number;
+interface StockMonthData extends DefaultAggregateData {
   items: StockItem[];
-  quantity: number;
 }
