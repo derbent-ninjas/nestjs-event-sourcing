358bc6a3351682faf8431d15808b84987237ddb2
TbotaPhantA <botakiller@mail.ru>
2024-11-12 23:54:20 +0000

    itemsWereShipped event was added

---
+++ src/storage/domain/aggregates/stockMonth/events/itemsWereShipped.ts
@@ -0,0 +1,20 @@
+import { StockItem } from '../stockItem';
+import { Event } from '../../../../../infrastructure/shared/utils/eventSourcing/event/event';
+
+export class ItemsWereShipped extends Event {
+  data: ItemsWereShippedEventData;
+  itemIdsSet: Set<string>;
+
+  constructor(raw: ItemsWereShipped) {
+    super(raw);
+    this.data = raw.data;
+    this.itemIdsSet = raw.data.items.reduce((acc, item) => {
+      acc.add(item.id);
+      return acc;
+    }, new Set<string>());
+  }
+}
+
+interface ItemsWereShippedEventData {
+  items: StockItem[];
+}
