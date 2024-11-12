fe4fab57b5ea47f12907c76f28820c7cef4c995f
TbotaPhantA <botakiller@mail.ru>
2024-11-12 00:01:48 +0000

    AggregateRoot was implemented

---
+++ src/storage/domain/aggregates/AggregateRoot.ts
@@ -0,0 +1,30 @@
+import { Entity } from './entity';
+import { IAggregate } from './IAggregate';
+import { Event } from '../events/event';
+import * as assert from 'assert';
+
+export abstract class AggregateRoot<Data extends AggregateData>
+  extends Entity<Data>
+  implements IAggregate
+{
+  apply(event: Event): void {
+    this.validateEvent(event);
+    this.transform(event);
+
+    this.__data.aggregateVersion++;
+  }
+
+  private validateEvent(event: Event): void {
+    assert.notEqual(event, undefined);
+    assert.strictEqual(event.aggregateId, this.__data.aggregateId);
+  }
+
+  abstract transform(event: Event): void;
+}
+
+type AggregateData = DefaultAggregateData & Record<string, any>;
+
+interface DefaultAggregateData {
+  aggregateId: string;
+  aggregateVersion: number;
+}
