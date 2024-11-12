e722d7f2beed1d89682bc3da81f7f73841463c89
TbotaPhantA <botakiller@mail.ru>
2024-11-12 00:03:38 +0000

    stockMonth aggregate root was implemented with single initial event

--- src/storage/domain/aggregates/stockMonth/stockMonth.ts
+++ src/storage/domain/aggregates/stockMonth/stockMonth.ts
@@ -1,26 +1,36 @@
 import { StockItem } from './stockItem';
-import { Importable } from '../../../../infrastructure/shared/types/importable';
-import { Exportable } from '../../../../infrastructure/shared/types/exportable';
+import { AggregateRoot } from '../AggregateRoot';
+import { Event } from '../../events/event';
+import { StockMonthWasOpenedEventData } from '../../events/openStockMonthEventData';
+import { exhaustiveTypeException } from 'tsconfig-paths/lib/try-path';
 
-export class StockMonth
-  implements Importable<StockMonthData>, Exportable<StockMonthData>
-{
-  private __data: StockMonthData;
-
-  constructor(data: StockMonthData) {
-    this.__data = data;
+export class StockMonth extends AggregateRoot<StockMonthData> {
+  transform(_event: Event): void {
+    if (StockMonthWasOpenedEventData.isThisEvent(_event)) {
+      this.transformStockMonthWasOpened(_event);
+    } else {
+      exhaustiveTypeException(_event);
+    }
   }
 
-  import(data: StockMonthData): void {
-    this.__data = data;
-  }
-
-  export(): StockMonthData {
-    return this.__data;
+  private transformStockMonthWasOpened(
+    _event: Event<StockMonthWasOpenedEventData>,
+  ) {
+    try {
+      this.__data.aggregateId = _event.aggregateId;
+      this.__data.aggregateVersion = _event.version;
+      this.__data.items = _event.data.items;
+      this.__data.quantity = _event.data.items.length;
+    } catch (e) {
+      console.log(`Error processing StockMonthWasOpenedEvent: ${e}`);
+      throw e;
+    }
   }
 }
 
 interface StockMonthData {
+  aggregateId: string;
+  aggregateVersion: number;
   items: StockItem[];
   quantity: number;
 }
