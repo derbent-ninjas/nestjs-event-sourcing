9612405c8661e5a4cccfb82ab4f34ea7e89e83d9
TbotaPhantA <botakiller@mail.ru>
2024-11-13 21:47:28 +0000

    swagger was added to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Warehousing (nestjs-event-sourcing)')
+    .setDescription('Api for the warehousing event-sourced app example')
+    .setVersion('1.0.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
