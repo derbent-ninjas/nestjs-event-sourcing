46033556293570cec5735c50f23701a120f8879e
TbotaPhantA <botakiller@mail.ru>
2024-11-14 23:08:52 +0000

    module fix

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -3,9 +3,12 @@ import { StockMonthController } from './application/stockMonth.controller';
 import { OpenStockMonthService } from './application/services/openStockMonth.service';
 import { RandomModule } from '../infrastructure/random/random.module';
 import { TimeModule } from '../infrastructure/time/time.module';
+import { StockMonthEventRepository } from './dal/stockMonthEventRepository.service';
+import { DBModule } from '../infrastructure/db/db.module';
 
 @Module({
-  imports: [RandomModule, TimeModule],
-  controllers: [StockMonthController, OpenStockMonthService],
+  imports: [RandomModule, TimeModule, DBModule],
+  controllers: [StockMonthController],
+  providers: [OpenStockMonthService, StockMonthEventRepository],
 })
 export class StorageModule {}
