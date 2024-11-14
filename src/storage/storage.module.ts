f7a3ba630297c18715ab7e60385c95ffcbc133fc
TbotaPhantA <botakiller@mail.ru>
2024-11-14 22:36:26 +0000

    time and random module were added to storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StockMonthController } from './application/stockMonth.controller';
+import { OpenStockMonthService } from './application/services/openStockMonth.service';
+import { RandomModule } from '../infrastructure/random/random.module';
+import { TimeModule } from '../infrastructure/time/time.module';
 
 @Module({
-  controllers: [StockMonthController],
+  imports: [RandomModule, TimeModule],
+  controllers: [StockMonthController, OpenStockMonthService],
 })
 export class StorageModule {}
