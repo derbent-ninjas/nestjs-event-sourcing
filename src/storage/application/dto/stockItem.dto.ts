96245408e5b3d4e107d64b688b5c3284ee3694bc
TbotaPhantA <botakiller@mail.ru>
2024-11-14 22:35:59 +0000

    id which should be generated at the frontend was added

--- src/storage/application/dto/stockItem.dto.ts
+++ src/storage/application/dto/stockItem.dto.ts
@@ -5,12 +5,18 @@ import {
   IsInt,
   IsNotEmpty,
   IsString,
+  IsUUID,
   Max,
   Min,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class StockItemDto {
+  @IsNotEmpty()
+  @IsUUID()
+  @ApiProperty({ example: 'a6334c2c-86db-4658-9660-1938afd126de' })
+  id!: string;
+
   @IsNotEmpty()
   @IsString()
   @ApiProperty({ example: 'Computer' })
