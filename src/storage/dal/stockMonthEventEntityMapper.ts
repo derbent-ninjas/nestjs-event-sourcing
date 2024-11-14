f625a4d32c7cffff62b9124f9cb809b179a598da
TbotaPhantA <botakiller@mail.ru>
2024-11-14 00:11:31 +0000

    `stockMonthEventEntityMapper` was implemented

---
+++ src/storage/dal/stockMonthEventEntityMapper.ts
@@ -0,0 +1,26 @@
+import { StockMonthEventEntity } from './stockMonthEventEntity';
+import { Event } from '../../infrastructure/shared/utils/eventSourcing/event/event';
+
+export class StockMonthEventEntityMapper {
+  static toDomain(entity: StockMonthEventEntity): Event {
+    return new Event(entity);
+  }
+
+  static toEntity(domain: Event): StockMonthEventEntity {
+    const entity = new StockMonthEventEntity();
+
+    entity.seqId = domain.seqId;
+    entity.eventId = domain.eventId;
+    entity.eventName = domain.eventName;
+    entity.aggregateId = domain.aggregateId;
+    entity.aggregateName = domain.aggregateName;
+    entity.contextName = domain.contextName;
+    entity.causationId = domain.causationId;
+    entity.correlationId = domain.correlationId;
+    entity.version = domain.version;
+    entity.data = domain.data;
+    entity.createdAt = domain.createdAt;
+
+    return entity;
+  }
+}
