72e752678e5e2b979256ad29a74a10e0847a51be
TbotaPhantA <botakiller@mail.ru>
2024-11-14 22:35:23 +0000

    call to service was implemented in controller

--- src/storage/application/stockMonth.controller.ts
+++ src/storage/application/stockMonth.controller.ts
@@ -2,13 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { OpenStockMonthDto } from './dto/openStockMonth/openStockMonth.dto';
 import { OpenStockMonthResponseDto } from './dto/openStockMonth/openStockMonthResponse.dto';
+import { OpenStockMonthService } from './services/openStockMonth.service';
 
 @Controller('storage/stock-month')
 @ApiTags('storage/stock-month')
 export class StockMonthController {
+  constructor(private readonly openStockMonthService: OpenStockMonthService) {}
+
   @Post('/open')
   @ApiResponse({ type: OpenStockMonthResponseDto })
   async openStockMonth(@Body() body: OpenStockMonthDto) {
-    return { body };
+    return this.openStockMonthService.runTransaction(body);
   }
 }
