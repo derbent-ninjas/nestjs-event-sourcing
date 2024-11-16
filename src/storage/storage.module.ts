2c6fe0897a18bdbeac20f464cbc5a8c9213f3063
TbotaPhantA <botakiller@mail.ru>
2024-11-16 01:08:06 +0000

    AddReceivedItemsService was added to module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -5,10 +5,15 @@ import { RandomModule } from '../infrastructure/random/random.module';
 import { TimeModule } from '../infrastructure/time/time.module';
 import { StockMonthEventRepository } from './dal/stockMonthEventRepository.service';
 import { DBModule } from '../infrastructure/db/db.module';
+import { AddReceivedItemsService } from './application/services/addReceivedItems.service';
 
 @Module({
   imports: [RandomModule, TimeModule, DBModule],
   controllers: [StockMonthController],
-  providers: [OpenStockMonthService, StockMonthEventRepository],
+  providers: [
+    OpenStockMonthService,
+    AddReceivedItemsService,
+    StockMonthEventRepository,
+  ],
 })
 export class StorageModule {}
