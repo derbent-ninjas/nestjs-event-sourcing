a6d7e494c49ef9d0d2349d95c40e1a344af2e8f6
TbotaPhantA <botakiller@mail.ru>
2024-11-16 01:07:52 +0000

    type assertion was added

--- src/storage/domain/aggregates/stockMonth/events/stockMonthWasOpened.ts
+++ src/storage/domain/aggregates/stockMonth/events/stockMonthWasOpened.ts
@@ -1,6 +1,7 @@
 import { Event } from '../../../../../infrastructure/shared/utils/eventSourcing/event/event';
 import { StockItem } from '../stockItem';
 import { MonthCodeEnum } from '../enums/monthCode.enum';
+import * as assert from 'assert';
 
 export class StockMonthWasOpened extends Event {
   data: StockMonthWasOpenedData;
@@ -15,4 +16,13 @@ interface StockMonthWasOpenedData {
   month: MonthCodeEnum;
   locationId: string;
   items: StockItem[];
+}
+
+export function assertEventIsStockMonthWasOpened(
+  event: Event,
+): asserts event is StockMonthWasOpened {
+  assert.ok(
+    event.eventName === StockMonthWasOpened.name,
+    `Event is not ${StockMonthWasOpened.name}`,
+  );
 }
