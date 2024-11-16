c7fa09bf29dae5593371eb6395f9ab3329199225
TbotaPhantA <botakiller@mail.ru>
2024-11-16 01:06:59 +0000

    mapping was improved, because it creates instance of events

--- src/storage/dal/stockMonthEventMapper.ts
+++ src/storage/dal/stockMonthEventMapper.ts
@@ -1,9 +1,35 @@
 import { StockMonthEventEntity } from './stockMonthEventEntity';
 import { Event } from '../../infrastructure/shared/utils/eventSourcing/event/event';
+import { StockMonthWasOpened } from '../domain/aggregates/stockMonth/events/stockMonthWasOpened';
+import { ItemsWereShipped } from '../domain/aggregates/stockMonth/events/itemsWereShipped';
+import { ItemsWereReceived } from '../domain/aggregates/stockMonth/events/itemsWereReceived';
+import { InventoryWasAdjusted } from '../domain/aggregates/stockMonth/events/inventoryWasAdjusted';
+import { StockMonthWasClosed } from '../domain/aggregates/stockMonth/events/stockMonthWasClosed';
+
+function isEventInstance<T extends Event>(
+  event: Event,
+  _class: new (...any: any[]) => T,
+): event is T {
+  return event.eventName === _class.name;
+}
 
 export class StockMonthEventMapper {
   static toDomain(entity: StockMonthEventEntity): Event {
-    return new Event(entity);
+    const event = new Event(entity);
+
+    if (isEventInstance(event, StockMonthWasOpened)) {
+      return new StockMonthWasOpened(event);
+    } else if (isEventInstance(event, ItemsWereReceived)) {
+      return new ItemsWereReceived(event);
+    } else if (isEventInstance(event, ItemsWereShipped)) {
+      return new ItemsWereShipped(event);
+    } else if (isEventInstance(event, InventoryWasAdjusted)) {
+      return new InventoryWasAdjusted(event);
+    } else if (isEventInstance(event, StockMonthWasClosed)) {
+      return new StockMonthWasClosed(event);
+    }
+
+    throw new Error(`Unknown event is being mapped: ${event.eventName}`);
   }
 
   static toEntity(domain: Event): StockMonthEventEntity {
