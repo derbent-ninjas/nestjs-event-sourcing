e5a71e137a8075e6c48c49f40f5e4b3613ade9d0
TbotaPhantA <botakiller@mail.ru>
2024-11-16 01:05:07 +0000

    only items are required

--- src/storage/domain/aggregates/stockMonth/utils/asserts/assertItemIdsAreUnique.ts
+++ src/storage/domain/aggregates/stockMonth/utils/asserts/assertItemIdsAreUnique.ts
@@ -2,7 +2,9 @@ import { OpenStockMonthDto } from '../../../../../application/dto/openStockMonth
 import { BadRequestException } from '@nestjs/common';
 import { ITEMS_MUST_HAVE_UNIQUE_UUIDS } from '../../../../../../infrastructure/shared/errorMessages';
 
-export const assertItemIdsAreUnique = (dto: OpenStockMonthDto) => {
+export const assertItemIdsAreUnique = (
+  dto: Pick<OpenStockMonthDto, 'items'>,
+) => {
   const itemIds = dto.items.map((item) => item.id);
   const uniqueItemIds = new Set(itemIds);
   if (uniqueItemIds.size !== itemIds.length) {
