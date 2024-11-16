e4bb800e37ee72616d75f58ea829985c1e149d77
TbotaPhantA <botakiller@mail.ru>
2024-11-16 01:06:03 +0000

    new endpoint /storage/stock-month/add-received-items was added

--- src/storage/application/stockMonth.controller.ts
+++ src/storage/application/stockMonth.controller.ts
@@ -3,15 +3,27 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { OpenStockMonthDto } from './dto/openStockMonth/openStockMonth.dto';
 import { OpenStockMonthResponseDto } from './dto/openStockMonth/openStockMonthResponse.dto';
 import { OpenStockMonthService } from './services/openStockMonth.service';
+import { AddReceivedItemsDto } from './dto/addReceivedItems.ts/addReceivedItems.dto';
+import { AddReceivedItemsResponseDto } from './dto/addReceivedItems.ts/addReceivedItemsResponse.dto';
+import { AddReceivedItemsService } from './services/addReceivedItems.service';
 
 @Controller('storage/stock-month')
 @ApiTags('storage/stock-month')
 export class StockMonthController {
-  constructor(private readonly openStockMonthService: OpenStockMonthService) {}
+  constructor(
+    private readonly openStockMonthService: OpenStockMonthService,
+    private readonly addReceivedItemsService: AddReceivedItemsService,
+  ) {}
 
   @Post('/open')
   @ApiResponse({ type: OpenStockMonthResponseDto })
   async openStockMonth(@Body() body: OpenStockMonthDto) {
     return this.openStockMonthService.runTransaction(body);
+  }
+
+  @Post('/add-received-items')
+  @ApiResponse({ type: AddReceivedItemsResponseDto })
+  async addReceivedItems(@Body() body: AddReceivedItemsDto) {
+    return this.addReceivedItemsService.runTransaction(body);
   }
 }
