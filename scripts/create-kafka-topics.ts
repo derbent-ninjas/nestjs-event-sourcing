65ab08303d32e6d2dd5b9a1c1a7bcab58dbba91e
TbotaPhantA <botakiller@mail.ru>
2024-11-18 22:32:22 +0000

    script for creating kafka topics was added

---
+++ scripts/create-kafka-topics.ts
@@ -0,0 +1,37 @@
+import { loadEnvironment } from '../src/infrastructure/shared/utils/loadEnvironment';
+
+loadEnvironment();
+import { Kafka } from 'kafkajs';
+import { config } from '../src/infrastructure/config/config';
+
+(async (): Promise<void> => {
+  const kafka = createKafka();
+  const admin = kafka.admin();
+
+  await admin.connect();
+
+  const toCreateTopics = [config.kafka.kafkaStockEventsTopic];
+
+  const areTopicsCreated = await admin.createTopics({
+    topics: toCreateTopics.map((topic) => ({
+      topic,
+      numPartitions: 1,
+      replicationFactor: 1,
+    })),
+  });
+
+  if (areTopicsCreated) {
+    console.log(`topics ${toCreateTopics} are successfully created`);
+  }
+
+  await admin.disconnect();
+
+  function createKafka(): Kafka {
+    return new Kafka({
+      clientId: 'acceptance-tests',
+      brokers: [
+        `${config.kafka.kafka1Host}:${config.kafka.kafka1ExternalPort}`,
+      ],
+    });
+  }
+})();
