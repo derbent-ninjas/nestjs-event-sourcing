890a4c5cdf8bcecb33caa8e6d1e3abb59f0ec606
TbotaPhantA <botakiller@mail.ru>
2024-11-18 23:01:42 +0000

    fastify was added

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from './infrastructure/config/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const document = new DocumentBuilder()
     .setTitle('Warehousing (nestjs-event-sourcing)')
