55fb4989b55ef0a3c23c726166a8039c70bd9fcc
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:15:44 +0000

    projection providers were added to storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { StockMonthController } from './application/stockMonth.controller';
-import { OpenStockMonthService } from './application/services/openStockMonth.service';
+import { OpenStockMonthService } from './application/services/projections/openStockMonth.service';
 import { RandomModule } from '../infrastructure/random/random.module';
 import { TimeModule } from '../infrastructure/time/time.module';
 import { StockMonthEventRepository } from './dal/stockMonthEventRepository.service';
 import { DBModule } from '../infrastructure/db/db.module';
-import { AddReceivedItemsService } from './application/services/addReceivedItems.service';
-import { RemoveShippedItemsService } from './application/services/removeShippedItems.service';
-import { HydrationService } from './application/services/hydration.service';
-import { AdjustInventoryService } from './application/services/adjustInventory.service';
+import { AddReceivedItemsService } from './application/services/projections/addReceivedItems.service';
+import { RemoveShippedItemsService } from './application/services/projections/removeShippedItems.service';
+import { HydrationService } from './application/services/projections/hydration.service';
+import { AdjustInventoryService } from './application/services/projections/adjustInventory.service';
+import { GetStockItemsService } from './application/services/query/getStockItems.service';
+import { StockProjectionRepository } from './dal/projections/stock-projection-repository.service';
+import { StockProjectionsService } from './application/services/commands/stockProjections.service';
 
 @Module({
   imports: [RandomModule, TimeModule, DBModule],
@@ -20,6 +23,9 @@ import { AdjustInventoryService } from './application/services/adjustInventory.s
     AdjustInventoryService,
     HydrationService,
     StockMonthEventRepository,
+    GetStockItemsService,
+    StockProjectionRepository,
+    StockProjectionsService,
   ],
 })
 export class StorageModule {}
