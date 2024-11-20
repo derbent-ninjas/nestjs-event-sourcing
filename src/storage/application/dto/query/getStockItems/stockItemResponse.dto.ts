36b806ad1765426956e99fd2a4922feaabaddc8e
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:13:51 +0000

    stockItemResponse.dto.ts was added

---
+++ src/storage/application/dto/query/getStockItems/stockItemResponse.dto.ts
@@ -0,0 +1,28 @@
+import { StockItemProjection } from '../../../../dal/projections/stockItem.projection';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+
+export class StockItemResponseDto extends OmitType(StockItemProjection, [
+  'stock',
+]) {
+  @ApiProperty({ name: '123' })
+  locationId!: string;
+
+  static from(item: StockItemProjection): StockItemResponseDto {
+    const dto = new StockItemResponseDto();
+
+    dto.id = item.id;
+    dto.itemName = item.itemName;
+    dto.description = item.description;
+    dto.isFlammable = item.isFlammable;
+    dto.isFragile = item.isFragile;
+    dto.temperatureMode = item.temperatureMode;
+    dto.weightGrams = item.weightGrams;
+    dto.inventoryAdjustment = item.inventoryAdjustment;
+    dto.createdAt = item.createdAt;
+    dto.updatedAt = item.updatedAt;
+    dto.removedAt = item.removedAt;
+    dto.locationId = item.stock.locationId;
+
+    return dto;
+  }
+}
