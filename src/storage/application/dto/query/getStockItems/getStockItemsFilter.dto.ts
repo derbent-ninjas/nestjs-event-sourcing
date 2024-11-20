48525b56fe9021f24f27bb07d550dd38f82600d0
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:10:03 +0000

    swagger additional docs

--- src/storage/application/dto/query/getStockItems/getStockItemsFilter.dto.ts
+++ src/storage/application/dto/query/getStockItems/getStockItemsFilter.dto.ts
@@ -7,44 +7,53 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
-import { TemperatureModeEnum } from '../../../domain/aggregates/stockMonth/enums/temperatureMode.enum';
-import { InventoryAdjustmentEnum } from '../../../domain/aggregates/stockMonth/enums/inventoryAdjustment.enum';
+import { TemperatureModeEnum } from '../../../../domain/aggregates/stockMonth/enums/temperatureMode.enum';
+import { InventoryAdjustmentEnum } from '../../../../domain/aggregates/stockMonth/enums/inventoryAdjustment.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetStockItemsFilterDto {
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
+  @ApiProperty()
   locationId?: string[];
 
   @IsOptional()
   @IsAlphanumeric()
+  @ApiProperty()
   itemName?: string;
 
   @IsOptional()
   @IsArray()
   @IsBoolean({ each: true })
+  @ApiProperty()
   isFlammable?: boolean[];
 
   @IsOptional()
   @IsArray()
   @IsBoolean({ each: true })
+  @ApiProperty()
   isFragile?: boolean[];
 
   @IsOptional()
   @IsArray()
   @IsEnum(TemperatureModeEnum, { each: true })
+  @ApiProperty()
   temperatureMode?: TemperatureModeEnum[];
 
   @IsOptional()
   @IsInt()
+  @ApiProperty()
   minWeight?: number;
 
   @IsOptional()
   @IsInt()
+  @ApiProperty()
   maxWeight?: number;
 
   @IsOptional()
   @IsArray()
   @IsEnum(InventoryAdjustmentEnum, { each: true })
+  @ApiProperty()
   inventoryAdjustment?: InventoryAdjustmentEnum[];
 }
