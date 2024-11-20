2b9c0189b4ef31f60093ede19fa23a68bc34b291
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:11:04 +0000

    data was exported and validate was added

--- src/storage/domain/aggregates/stockMonth/events/inventoryWasAdjusted.ts
+++ src/storage/domain/aggregates/stockMonth/events/inventoryWasAdjusted.ts
@@ -1,9 +1,11 @@
 import { Event } from '../../../../../infrastructure/shared/utils/eventSourcing/event/event';
 import { StockItem } from '../stockItem';
 import { NoMethods } from '../../../../../infrastructure/shared/types/noMethods';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class InventoryWasAdjusted extends Event {
-  data: InventoryWasAdjustedEventData;
+  data: InventoryWasAdjustedData;
   private readonly _shortageItemsIdsSet: Set<string>;
 
   constructor(raw: NoMethods<InventoryWasAdjusted>) {
@@ -20,7 +22,18 @@ export class InventoryWasAdjusted extends Event {
   }
 }
 
-interface InventoryWasAdjustedEventData {
-  surplusItems: StockItem[];
-  shortageItems: StockItem[];
+export class InventoryWasAdjustedData {
+  @IsNotEmpty()
+  @IsString()
+  locationId!: string;
+
+  @IsNotEmpty()
+  @Type(() => StockItem)
+  @ValidateNested({ each: true })
+  surplusItems!: StockItem[];
+
+  @IsNotEmpty()
+  @Type(() => StockItem)
+  @ValidateNested({ each: true })
+  shortageItems!: StockItem[];
 }
