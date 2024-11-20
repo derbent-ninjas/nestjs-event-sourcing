750966b7b54af7a63578d79cacb14a325acfb49a
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:10:15 +0000

    total was added to get response for pagination

--- src/storage/application/dto/query/getStockItems/getStockItemsResponse.dto.ts
+++ src/storage/application/dto/query/getStockItems/getStockItemsResponse.dto.ts
@@ -1,12 +1,16 @@
 import { StockItemResponseDto } from './stockItemResponse.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { NoMethods } from '../../../../infrastructure/shared/types/noMethods';
+import { NoMethods } from '../../../../../infrastructure/shared/types/noMethods';
 
 export class GetStockItemsResponseDto {
   @ApiProperty({ type: [StockItemResponseDto] })
   items: StockItemResponseDto[];
 
+  @ApiProperty()
+  total: number;
+
   constructor(raw: NoMethods<GetStockItemsResponseDto>) {
     this.items = raw.items;
+    this.total = raw.total;
   }
 }
