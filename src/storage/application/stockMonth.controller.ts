51a740f93716e296c503d30d753bab1b37a2a093
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:14:16 +0000

    get endpoint and kafka consumption were added to controller

--- src/storage/application/stockMonth.controller.ts
+++ src/storage/application/stockMonth.controller.ts
@@ -1,27 +1,49 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { OpenStockMonthDto } from './dto/openStockMonth/openStockMonth.dto';
-import { OpenStockMonthResponseDto } from './dto/openStockMonth/openStockMonthResponse.dto';
-import { OpenStockMonthService } from './services/openStockMonth.service';
-import { AddReceivedItemsDto } from './dto/addReceivedItems.ts/addReceivedItems.dto';
-import { AddReceivedItemsResponseDto } from './dto/addReceivedItems.ts/addReceivedItemsResponse.dto';
-import { AddReceivedItemsService } from './services/addReceivedItems.service';
-import { RemoveShippedItemsDto } from './dto/removeShippedItems/removeShippedItems.dto';
-import { RemoveShippedItemsResponseDto } from './dto/removeShippedItems/removeShippedItemsResponse.dto';
-import { RemoveShippedItemsService } from './services/removeShippedItems.service';
-import { AdjustInventoryDto } from './dto/adjustInventory/adjustInventory.dto';
-import { AdjustInventoryResponseDto } from './dto/adjustInventory/adjustInventoryResponse.dto';
-import { AdjustInventoryService } from './services/adjustInventory.service';
+import { OpenStockMonthDto } from './dto/commands/openStockMonth/openStockMonth.dto';
+import { OpenStockMonthResponseDto } from './dto/commands/openStockMonth/openStockMonthResponse.dto';
+import { OpenStockMonthService } from './services/projections/openStockMonth.service';
+import { AddReceivedItemsDto } from './dto/commands/addReceivedItems.ts/addReceivedItems.dto';
+import { AddReceivedItemsResponseDto } from './dto/commands/addReceivedItems.ts/addReceivedItemsResponse.dto';
+import { AddReceivedItemsService } from './services/projections/addReceivedItems.service';
+import { RemoveShippedItemsDto } from './dto/commands/removeShippedItems/removeShippedItems.dto';
+import { RemoveShippedItemsResponseDto } from './dto/commands/removeShippedItems/removeShippedItemsResponse.dto';
+import { RemoveShippedItemsService } from './services/projections/removeShippedItems.service';
+import { AdjustInventoryDto } from './dto/commands/adjustInventory/adjustInventory.dto';
+import { AdjustInventoryResponseDto } from './dto/commands/adjustInventory/adjustInventoryResponse.dto';
+import { AdjustInventoryService } from './services/projections/adjustInventory.service';
+import { GetStockItemsDto } from './dto/query/getStockItems/getStockItems.dto';
+import { GetStockItemsResponseDto } from './dto/query/getStockItems/getStockItemsResponse.dto';
+import { GetStockItemsService } from './services/query/getStockItems.service';
+import { Ctx, EventPattern, KafkaContext } from '@nestjs/microservices';
+import { config } from '../../infrastructure/config/config';
+import { plainToInstance } from 'class-transformer';
+import { IHeaders } from 'kafkajs';
+import { MessageHeadersDto } from './dto/messages/messageHeaders.dto';
+import { validate } from 'class-validator';
+import { assertIsNotEmpty } from '../../infrastructure/shared/assertIsNotEmpty';
+import { StockProjectionsService } from './services/commands/stockProjections.service';
+import { inspect } from 'util';
 
 @Controller('storage/stock-month')
 @ApiTags('storage/stock-month')
 export class StockMonthController {
   constructor(
+    private readonly getStockItemsService: GetStockItemsService,
     private readonly openStockMonthService: OpenStockMonthService,
     private readonly addReceivedItemsService: AddReceivedItemsService,
     private readonly removeShippedItemsService: RemoveShippedItemsService,
     private readonly adjustInventoryService: AdjustInventoryService,
+    private readonly stockProjectionsService: StockProjectionsService,
   ) {}
+
+  @Post('/get-stock-items')
+  @ApiResponse({ type: GetStockItemsResponseDto })
+  async getStockItems(
+    @Body() dto: GetStockItemsDto,
+  ): Promise<GetStockItemsResponseDto> {
+    return this.getStockItemsService.getStockItems(dto);
+  }
 
   @Post('/open')
   @ApiResponse({ type: OpenStockMonthResponseDto })
@@ -48,5 +70,58 @@ export class StockMonthController {
     @Body() body: AdjustInventoryDto,
   ): Promise<AdjustInventoryResponseDto> {
     return this.adjustInventoryService.runTransaction(body);
+  }
+
+  @EventPattern(config.kafka.kafkaStockEventsTopic)
+  async projectStock(@Ctx() context: KafkaContext): Promise<void> {
+    try {
+      const message = context.getMessage();
+      const headers = await this.validateHeaders(message.headers);
+      const value = this.convertBufferToPlain(message.value);
+      console.log(inspect({ headers, value }, { depth: 15 }));
+      return this.stockProjectionsService.project(headers, value);
+    } catch (error) {
+      console.log(inspect({ error }, { depth: 15 }));
+    }
+  }
+
+  private async validateHeaders(
+    headerBuffers: IHeaders | undefined,
+  ): Promise<MessageHeadersDto> {
+    const plainHeaders = this.headerBuffersToPlain(headerBuffers);
+    const headers = plainToInstance(MessageHeadersDto, plainHeaders);
+    const errors = await validate(headers);
+
+    if (errors.length > 0) {
+      throw new Error(errors.join('\n'));
+    }
+
+    return headers;
+  }
+
+  private headerBuffersToPlain(headerBuffers: IHeaders | undefined): {
+    [key: string]: string | unknown;
+  } {
+    assertIsNotEmpty(headerBuffers);
+    const plainHeaders: { [key: string]: string | unknown } = {};
+
+    for (const key in headerBuffers) {
+      if (Buffer.isBuffer(headerBuffers[key])) {
+        plainHeaders[key] = (headerBuffers[key] as Buffer).toString();
+      } else {
+        plainHeaders[key] = headerBuffers[key];
+      }
+    }
+
+    return plainHeaders;
+  }
+
+  private convertBufferToPlain(valueBuffer: Buffer | null): {
+    [key: string]: any;
+  } {
+    assertIsNotEmpty(valueBuffer);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return JSON.parse(valueBuffer.payload);
   }
 }
