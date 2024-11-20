d32135613b2cf6e272dbd343229abaeb21fba15f
TbotaPhantA <botakiller@mail.ru>
2024-11-20 01:09:30 +0000

    additinoal swagger docs

--- src/storage/application/dto/query/getStockItems/getStockItems.dto.ts
+++ src/storage/application/dto/query/getStockItems/getStockItems.dto.ts
@@ -1,8 +1,11 @@
 import { GetStockItemsFilterDto } from './getStockItemsFilter.dto';
 import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { PaginationDto } from '../../pagination.dto';
 
-export class GetStockItemsDto {
+export class GetStockItemsDto extends PaginationDto {
   @IsNotEmpty()
   @ValidateNested()
+  @ApiProperty({ type: GetStockItemsFilterDto })
   filter!: GetStockItemsFilterDto;
 }
